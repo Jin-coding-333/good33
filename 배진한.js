0716322954dc440abc5b3f462d2a5f40b64c138a
Jinhan <wlsgks333@naver.com>
2024-10-19 22:25:22 +0900

    이것은 테스트용 커밋 입니다. hello 변수를 선언하고 값을 word로 주었습니다. PR의 3가지 방법을 보기 위해 시험 삼아 하고 있습니다.

--- 배진한.js
+++ 배진한.js
@@ -1,1 +1,14 @@
-"안녕하세요"
+let grid = document.querySelector('body')
+
+grid.innerHTML += '음..ㄹㅇㄹㅇㄹㅇㄹ'
+
+let text = document.querySelector('.text');
+
+text.textContent += "dfdl;sdffdfffsfsdffsdfefefskjdfljsladfjl"
+
+let main = grid.document.createElement('div');
+
+main.textContent = '메인 태그 생성';
+grid.prepend(first);
+
+let hello = word;
